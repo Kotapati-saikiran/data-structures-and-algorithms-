938804cb3a72fd06f99f66716f4e384da30de9b4
Kotapati Sai Kiran <kotapatisaikiran085@gmail.com>
2026-07-02 16:08:48 +0530

    added anothe lc sol

---
+++ leetcode/stack/224. Basic calculator.py
@@ -0,0 +1,43 @@
+s = "(1+(4+5+2)-3)+(6+8)"
+num = 0
+sign = "+"
+result = 0
+stack = []
+for ch in s:
+    if ch.isdigit():
+        num = num * 10 + int(ch)
+    elif ch == "+":
+        if sign == "+":
+            result += num
+        else:
+            result -= num
+        num = 0
+        sign = "+"
+    elif ch == "-":
+        if sign == "+":
+            result += num
+        else:
+            result -= num
+        num = 0
+        sign = "-"
+    elif ch == "(":
+        stack.append((result, sign))
+        result = 0
+        sign = "+"
+        num = 0
+    elif ch == ")":
+        if sign == "+":
+            result += num
+        else:
+            result -= num
+        prev_res, prev_sign = stack.pop()
+        if prev_sign == "+":
+            result = prev_res + result
+        else:
+            result = prev_res - result
+        num = 0
+if sign == "+":
+    result += num
+else:
+    result -= num
+print(result)
