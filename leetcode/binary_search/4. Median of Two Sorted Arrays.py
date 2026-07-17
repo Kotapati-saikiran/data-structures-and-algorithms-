5b228bb2446fdcfed273703e01b76e08eb17e646
Kotapati Sai Kiran <kotapatisaikiran085@gmail.com>
2026-07-17 19:56:43 +0530

    added another lc sol

---
+++ leetcode/binary_search/4. Median of Two Sorted Arrays.py
@@ -0,0 +1,12 @@
+def search(nums1, nums2):
+    merged = sorted(nums1 + nums2)
+    mid = len(merged) // 2
+    if len(merged) % 2 == 0:
+        x = (merged[mid] + merged[mid - 1]) / 2
+    else:
+        x = merged[mid]
+    return x
+
+nums1 = [1,3]
+nums2 = [2]
+print(search(nums1, nums2))
